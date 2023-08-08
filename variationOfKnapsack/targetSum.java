2c90b2e929841fdf49b6b696df905a276a5d8dc7
aminul islam <77560208+apna-tension@users.noreply.github.com>
2023-08-08 12:52:48 +0000

    calculate the target sum array

---
+++ variationOfKnapsack/targetSum.java
@@ -0,0 +1,35 @@
+// public package variationOfKnapsack;
+import java.util.Arrays;
+public class targetSum {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3};
+        int diff = 1;
+        int sum = 7; // sum of array;
+        sum = (sum+diff)/2;
+        System.out.println(tabulation(arr, sum));
+    }
+    public static int tabulation(int[] arr, int sum) {
+        int[][] dp = new int[arr.length+1][sum+1];
+        for (int i = 0; i < arr.length+1; i++) {
+            for (int j = 0; j < sum+1; j++) {
+                if (i == 0) dp[i][j] = 0;
+                if (j == 0) dp[i][j] = 1;
+            }
+        }
+
+        for (int i = 1; i < arr.length+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                if (arr[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+        return dp[arr.length][sum];
+    }
+    
+}
